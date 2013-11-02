b43caa786e6e09b904dbedb0697e73620c5f6e9c
Latrium1997 <perezeitor.alex434@hotmail.com>
2013-11-02 16:50:33 +0100

    estamos haciendo una pagina en la que puedes comentar y sale la hora a la que comentas, además, si te equivocas puedes eliminar el comentario.

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-/**
- * Created by root on 11/2/13.
- */
+
+$(document).ready(function(){
+    $("#enviar").on("click",function(){
+        var texto=$("#comentarios").val();
+        var fecha= new Date();
+        var fechaComentario= "<span class='fecha'>"+fecha.getHours()+":"+fecha.getMinutes()+":"+fecha.getSeconds()+"</span>";
+        var divComentarioEscrito="<div class='comentario-enviado'>";
+        var divCompleto=divComentarioEscrito+texto+fechaComentario+"<img class='comentario-eliminado' src='closebutton'>"+"</img>"+"</div>";
+        $("#comentarioescrito").prepend(divCompleto);
+
+    });
+
+});
